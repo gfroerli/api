a979a8409c12399827c53659f92a24329ed29b27
Josua Schmid <josua.schmid@renuo.ch>
2016-02-22 22:21:02 +0100

    provide root page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+  def root
+    render plain: 'This is the coredump water sensor api. See https://coredump.ch for contact information.'
+  end
 end
