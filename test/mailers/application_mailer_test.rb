598c33887f2d7f0628d9b18ecde0723725c6355a
Josua Schmid <josua.schmid@renuo.ch>
2016-02-22 22:53:32 +0100

    add base test for active job

--- test/mailers/application_mailer_test.rb
+++ test/mailers/application_mailer_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class ApplicationMailerTest < ActionMailer::TestCase
-  test 'should load the configuration' do
+  test 'that mailer can be extended' do
     class SampleMailer < ApplicationMailer; end
   end
 end
