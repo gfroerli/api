d8df052af2e1cff7bcdde95e9bd65bff285909ab
Josua Schmid <josua.schmid@renuo.ch>
2016-03-27 20:53:18 +0200

    add more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,12 @@ sensor = Sensor.create! device_name: 'photon_device_45',
                         location: { latitude: 47.222578, longitude: 8.814744 },
                         sponsor: sponsor
 
-_measurement = Measurement.create! temperature: 5.45682,
-                                   custom_attributes: Hash[sample_reading: '5.645V'],
-                                   sensor: sensor
+Measurement.create! [{temperature: 5.45682, custom_attributes: Hash[sample_reading: '5.645V'], sensor: sensor},
+                     {temperature: 6.45443, custom_attributes: Hash[sample_reading: '5.634V'], sensor: sensor},
+                     {temperature: 5.23145, custom_attributes: Hash[sample_reading: '5.622V'], sensor: sensor},
+                     {temperature: 3.43474, custom_attributes: Hash[sample_reading: '5.632V'], sensor: sensor},
+                     {temperature: 3.56567, custom_attributes: Hash[sample_reading: '5.644V'], sensor: sensor},
+                     {temperature: 3.56567, custom_attributes: Hash[sample_reading: '5.612V'], sensor: sensor},
+                     {temperature: 3.89346, custom_attributes: Hash[sample_reading: '5.611V'], sensor: sensor},
+                     {temperature: 4.45565, custom_attributes: Hash[sample_reading: '5.617V'], sensor: sensor},
+                     {temperature: 4.65767, custom_attributes: Hash[sample_reading: '5.676V'], sensor: sensor}]
