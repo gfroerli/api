1f5df3c7f69e7229e6637ebcde7f6289be1671e4
Josua Schmid <josua.schmid@renuo.ch>
2016-03-27 21:47:33 +0200

    make seeds more natural

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,42 @@
-sponsor = Sponsor.create! name: 'Hochschule für Technik Rapperswil',
-                          description: 'Die HSR ist tätig in den beiden Bereichen Technik und Informationstechnologie sowie Architektur, Bau- und Planungswesen.',
-                          active: true
+seeds = [
+          {
+              name: 'Hochschule für Technik Rapperswil',
+              description: 'Die HSR ist tätig in den Bereichen Technik und Informationstechnologie.',
+              sensor_prefix: 'HSR Mark '
+          },
+          {
+              name: 'Badi Lachen',
+              description: 'Wunderbar und an einzigartiger Lage am oberen Zürichsee gelegen.',
+              sensor_prefix: 'Boje T'
+          },
+          {
+              name: 'Hotel Schwanen',
+              description: 'The Hotel Schwanen in Rapperswil has a proud tradition.',
+              sensor_prefix: 'Swan '
+          },
+          {
+              name: 'Seebad Enge',
+              description: 'Das Seebad Enge wird seit Frühjahr 1999 von der Tonttu GmbH privat betrieben. ',
+              sensor_prefix: 'Badi '
+          },
+          {
+              name: 'Strandbad Küsnacht',
+              description: 'Seebad mit ca. 30 m Sandstrand am Zürichsee',
+              sensor_prefix: 'Goldküste Sensor '
+          },
+      ]
 
-sensor = Sensor.create! device_name: 'photon_device_45',
-                        caption: 'HSR Mark I',
-                        location: { latitude: 47.222578, longitude: 8.814744 },
-                        sponsor: sponsor
+seeds.each_with_index do |seed, i|
+  sponsor = Sponsor.create! name: seed[:name],
+                            description: seed[:description],
+                            active: true
 
-Measurement.create! [{temperature: 5.45682, custom_attributes: Hash[sample_reading: '5.645V'], sensor: sensor},
-                     {temperature: 6.45443, custom_attributes: Hash[sample_reading: '5.634V'], sensor: sensor},
-                     {temperature: 5.23145, custom_attributes: Hash[sample_reading: '5.622V'], sensor: sensor},
-                     {temperature: 3.43474, custom_attributes: Hash[sample_reading: '5.632V'], sensor: sensor},
-                     {temperature: 3.56567, custom_attributes: Hash[sample_reading: '5.644V'], sensor: sensor},
-                     {temperature: 3.56567, custom_attributes: Hash[sample_reading: '5.612V'], sensor: sensor},
-                     {temperature: 3.89346, custom_attributes: Hash[sample_reading: '5.611V'], sensor: sensor},
-                     {temperature: 4.45565, custom_attributes: Hash[sample_reading: '5.617V'], sensor: sensor},
-                     {temperature: 4.65767, custom_attributes: Hash[sample_reading: '5.676V'], sensor: sensor}]
+  sensor = Sensor.create! device_name: "photon_device_#{i}",
+                          caption: "#{seed[:sensor_prefix]}#{i}",
+                          location: { latitude: 47+(i/100), longitude: 8+(i/100) },
+                          sponsor: sponsor
+
+  50.times do
+    Measurement.create! temperature: rand(3000)/100, custom_attributes: Hash[sample_reading: '5.645V'], sensor: sensor
+  end
+end
