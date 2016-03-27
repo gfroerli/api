f08c6db951a0c3b4f5b8923901bf9d06b184ddf5
Josua Schmid <josua.schmid@renuo.ch>
2016-03-27 20:17:13 +0200

    extend cors config

--- config/application.rb
+++ config/application.rb
@@ -17,12 +17,5 @@ Bundler.require(*Rails.groups)
 module CoredumpWaterSensorApi
   class Application < Rails::Application
     config.api_only = true
-
-    config.middleware.insert_before 0, 'Rack::Cors' do
-      allow do
-        origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
-      end
-    end
   end
 end
