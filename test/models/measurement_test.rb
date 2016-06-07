1a1e68a2877729a1168f37af2e1564c772d376bf
Josua Schmid <josua.schmid@renuo.ch>
2016-06-07 21:23:37 +0200

    add simple test counting the results of the sliding window

--- test/models/measurement_test.rb
+++ test/models/measurement_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class MeasurementTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should scope measurements per sensor' do
+    create_list(:measurement, 3, sensor: create(:sensor))
+    create_list(:measurement, 3, sensor: create(:sensor))
+    create_list(:measurement, 3, sensor: create(:sensor))
+
+    measurements = Measurement.last_per_sensor(2).to_a
+    assert_equal(6, measurements.count)
+  end
 end
