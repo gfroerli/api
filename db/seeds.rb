e62c8127094f62d64aecb2319864ea5eaa174c7d
Danilo Bargen <mail@dbrgn.ch>
2016-07-01 21:50:02 +0200

    Add proper locations to db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,27 +2,32 @@ seeds = [
           {
               name: 'Hochschule für Technik Rapperswil',
               description: 'Die HSR ist tätig in den Bereichen Technik und Informationstechnologie.',
-              sensor_prefix: 'HSR Mark '
+              sensor_prefix: 'HSR Mark ',
+              location: { latitude: 47.222196, longitude: 8.815424 }
           },
           {
               name: 'Badi Lachen',
               description: 'Wunderbar und an einzigartiger Lage am oberen Zürichsee gelegen.',
-              sensor_prefix: 'Boje T'
+              sensor_prefix: 'Boje T',
+              location: { latitude: 47.196832, longitude: 8.851580 }
           },
           {
               name: 'Hotel Schwanen',
               description: 'The Hotel Schwanen in Rapperswil has a proud tradition.',
-              sensor_prefix: 'Swan '
+              sensor_prefix: 'Swan ',
+              location: { latitude: 47.225729, longitude: 8.813175 }
           },
           {
               name: 'Seebad Enge',
               description: 'Das Seebad Enge wird seit Frühjahr 1999 von der Tonttu GmbH privat betrieben. ',
-              sensor_prefix: 'Badi '
+              sensor_prefix: 'Badi ',
+              location: { latitude: 47.361576, longitude: 8.536855 }
           },
           {
               name: 'Strandbad Küsnacht',
               description: 'Seebad mit ca. 30 m Sandstrand am Zürichsee',
-              sensor_prefix: 'Goldküste Sensor '
+              sensor_prefix: 'Goldküste Sensor ',
+              location: { latitude: 47.309323, longitude: 8.583232 }
           },
       ]
 
@@ -33,7 +38,7 @@ seeds.each_with_index do |seed, i|
 
   sensor = Sensor.create! device_name: "photon_device_#{i}",
                           caption: "#{seed[:sensor_prefix]}#{i}",
-                          location: { latitude: 47+(i/100), longitude: 8+(i/100) },
+                          location: seed[:location],
                           sponsor: sponsor
 
   50.times do
