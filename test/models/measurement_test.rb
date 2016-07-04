d74ec409384659aad9f60d069a5213ca4feae20d
Josua Schmid <josua.schmid@renuo.ch>
2016-07-04 21:41:40 +0200

    Add test for overgrouping

--- test/models/measurement_test.rb
+++ test/models/measurement_test.rb
@@ -1,12 +1,21 @@
 require 'test_helper'
 
 class MeasurementTest < ActiveSupport::TestCase
-  test 'should scope measurements per sensor' do
+  def create_nine_sensor_measurements
     create_list(:measurement, 3, sensor: create(:sensor))
     create_list(:measurement, 3, sensor: create(:sensor))
     create_list(:measurement, 3, sensor: create(:sensor))
+  end
 
+  test 'should scope measurements per sensor' do
+    create_nine_sensor_measurements
     measurements = Measurement.last_per_sensor(2).to_a
     assert_equal(6, measurements.count)
+  end
+
+  test 'should scope measurements per sensor with limit (9 present, grouped by 4)' do
+    create_nine_sensor_measurements
+    measurements = Measurement.last_per_sensor(4).to_a
+    assert_equal(9, measurements.count)
   end
 end
