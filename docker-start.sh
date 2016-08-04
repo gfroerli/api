d5cd8e2d5aa45d4e9e508c1e553952c6509ca661
Danilo Bargen <mail@dbrgn.ch>
2016-08-04 22:38:37 +0200

    Add docker-compose setup

---
+++ docker-start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+PG_HOST=db
+PG_PORT=5432
+
+echo "Wait for postgres server to be ready..."
+while ! nc -q 1 $PG_HOST $PG_PORT </dev/null; do sleep 1; done
+
+bin/rails db:migrate
+bin/bundle exec puma -C config/puma.rb -b tcp://0.0.0.0:3000
