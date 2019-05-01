308fd234fef80632b06248cfc10c520a40e324a0
Josua Schmid <josua.schmid@renuo.ch>
2019-05-01 09:29:42 +0200

    Bundle update

--- test/factories/api_consumers.rb
+++ test/factories/api_consumers.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :api_consumer do
     public_api_key { SecureRandom.hex }
-    contact_email 'admin@exampleapp.com'
+    contact_email { 'admin@exampleapp.com' }
 
     factory :private_api_consumer do
       private_api_key { SecureRandom.hex }
--- test/factories/sensors.rb
+++ test/factories/sensors.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :sensor do
-    device_name 'photon_device_nr4562'
-    caption 'HSR Mark I'
-    latitude 47.222578
-    longitude 8.814744
+    device_name { 'photon_device_nr4562' }
+    caption { 'HSR Mark I' }
+    latitude { 47.222578 }
+    longitude { 8.814744 }
     sponsor
   end
 end
