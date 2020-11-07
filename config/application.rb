b11c51f2ab91df7315204f19a83a3e9e7f871805
Josua Schmid <josua.schmid@renuo.ch>
2020-11-07 14:33:11 +0100

    Update to Rails 6

--- config/application.rb
+++ config/application.rb
@@ -5,11 +5,11 @@ require "rails"
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
-require "active_storage/engine"
+# require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
-require "action_cable/engine"
+# require "action_cable/engine"
 # require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
@@ -20,7 +20,7 @@ Bundler.require(*Rails.groups)
 module CoredumpWaterSensorApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults "6.0"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
