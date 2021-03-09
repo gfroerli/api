bccc4233bd380fcd00d02ab10671cb2b7f1d745b
Josua Schmid <josua.schmid@renuo.ch>
2021-03-09 22:35:01 +0100

    Change non-null of contact-email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_213659) do
+ActiveRecord::Schema.define(version: 2021_03_09_212740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2020_11_16_213659) do
   create_table "api_consumers", id: :serial, force: :cascade do |t|
     t.string "public_api_key"
     t.string "private_api_key"
-    t.string "contact_email"
+    t.string "contact_email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "caption"
