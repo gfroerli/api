a2faacd3ab8efb5104e84ed3156764e882c9a12f
Raphael Nestler <raphael.nestler@gmail.com>
2023-02-05 17:24:39 +0100

    Rubocop fixes

--- app/dashboards/waterbody_dashboard.rb
+++ app/dashboards/waterbody_dashboard.rb
@@ -1,4 +1,4 @@
-require "administrate/base_dashboard"
+require 'administrate/base_dashboard'
 
 class WaterbodyDashboard < Administrate::BaseDashboard
   # ATTRIBUTE_TYPES
@@ -15,7 +15,7 @@ class WaterbodyDashboard < Administrate::BaseDashboard
     name: Field::String,
     sensors: Field::HasMany,
     created_at: Field::DateTime,
-    updated_at: Field::DateTime,
+    updated_at: Field::DateTime
   }.freeze
 
   # COLLECTION_ATTRIBUTES
--- test/models/waterbody_test.rb
+++ test/models/waterbody_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class WaterbodyTest < ActiveSupport::TestCase
   # test "the truth" do
