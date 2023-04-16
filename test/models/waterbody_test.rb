b0d1b34c8540e89da93b0afd1b1dfcbda309e33a
Raphael Nestler <raphael.nestler@gmail.com>
2023-04-16 20:41:26 +0200

    Add waterbody model test

--- test/models/waterbody_test.rb
+++ test/models/waterbody_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class WaterbodyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should nullify sensor waterbody references on destroy' do
+    sensor = create(:sensor)
+    waterbody = create(:waterbody, sensors: [sensor])
+    assert_equal sensor.waterbody, waterbody
+    waterbody.destroy
+    assert_nil sensor.reload.waterbody
+  end
 end
