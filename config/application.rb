4d9b3aee5c1bb3e433bfa89461f1c9aa4c1617c2
Raphael Nestler <raphael.nestler@gmail.com>
2025-01-10 18:31:14 +0100

    Apply 7.0 default config and add cookie rotator (#235)
    See https://guides.rubyonrails.org/v7.0/upgrading_ruby_on_rails.html#key-generator-digest-class-changing-to-use-sha256

--- config/application.rb
+++ config/application.rb
@@ -21,8 +21,7 @@ Bundler.require(*Rails.groups)
 module CoredumpWaterSensorApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
-    config.active_support.cache_format_version = 7.0 # Can be removed when changing load_defaults to 7.0
+    config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
     #
