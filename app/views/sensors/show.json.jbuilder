d2efcef2f96a8297744719b1427e6cc15a2638b8
Raphael Nestler <raphael.nestler@gmail.com>
2025-07-19 00:05:51 +0200

    Add shortname to json responses

--- app/views/sensors/show.json.jbuilder
+++ app/views/sensors/show.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! @sensor, :id, :device_name, :caption, :latitude, :longitude,
+json.extract! @sensor, :id, :device_name, :shortname, :caption, :latitude, :longitude,
               :sponsor_id, :created_at, :updated_at
--- test/controllers/sensors_controller_test.rb
+++ test/controllers/sensors_controller_test.rb
@@ -20,7 +20,7 @@ class SensorsControllerTest < ActionDispatch::IntegrationTest
     get sensor_url(@sensor), env: public_auth_header
     assert_response :success
     assert_equal response.parsed_body.keys,
-                 %w[id device_name caption latitude longitude sponsor_id created_at updated_at]
+                 %w[id device_name shortname caption latitude longitude sponsor_id created_at updated_at]
   end
 
   test 'should create sensor' do
