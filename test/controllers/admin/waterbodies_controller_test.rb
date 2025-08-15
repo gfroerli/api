9ccf848e20176cf786cc5f2da076907fdc3fe85f
Raphael Nestler <raphael.nestler@gmail.com>
2025-08-15 12:02:02 +0200

    Extend request test to include ressource display_names

--- test/controllers/admin/waterbodies_controller_test.rb
+++ test/controllers/admin/waterbodies_controller_test.rb
@@ -13,5 +13,12 @@ class WaterbodiesControllerTest < ActionDispatch::IntegrationTest
   test 'should get success with authentication' do
     get admin_waterbodies_url, headers: @headers
     assert_response :success
+    assert_includes @response.body, 'Waterbodies'
+  end
+
+  test 'show an existing waterbody' do
+    waterbody = create(:waterbody, name: 'Zürichsee')
+    get admin_waterbody_url(waterbody), headers: @headers
+    assert_includes @response.body, "Show #{waterbody.name}"
   end
 end
